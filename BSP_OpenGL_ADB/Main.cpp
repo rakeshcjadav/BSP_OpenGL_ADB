fd3a19c3b06a15fdb298ac27e6094e4448dd0291
Rakesh Jadav <rakeshcjadav@gmail.com>
2021-10-16 12:28:05 +0530

    Rendering quad using shader & Texture

--- BSP_OpenGL_ADB/Main.cpp
+++ BSP_OpenGL_ADB/Main.cpp
@@ -2,23 +2,79 @@
 #include"GL/glew.h"
 #include"GLFW/glfw3.h"
 
+#define STB_IMAGE_IMPLEMENTATION
+#include"stb_image.h"
+
+std::string GetEnv(std::string strName)
+{
+    char* libvar;
+    size_t requiredSize;
+    getenv_s(&requiredSize, nullptr, 0, strName.c_str());
+    if (requiredSize == 0)
+    {
+        //   LogMessage(fmt::format("Environment variable {0} doesn't exist!", varName).c_str());
+        return nullptr;
+    }
+
+    libvar = (char*)malloc(requiredSize * sizeof(char));
+    if (!libvar)
+    {
+        //   LogMessage(fmt::format("Failed to allocate memory {0} bytes!", requiredSize).c_str());
+        return nullptr;
+    }
+
+    // Get the value of the LIB environment variable.
+    getenv_s(&requiredSize, libvar, requiredSize, strName.c_str());
+
+    return libvar;
+}
+
+std::string GetMediaPath()
+{
+    return GetEnv("MEDIA_PATH") + "\\";
+}
+
+std::string GetTexturePath()
+{
+    return GetMediaPath() + "textures\\";
+}
+
 void OnWindowResize(GLFWwindow * pWindow, int width, int height)
 {
     glViewport(0, 0, width, height);
     glScissor(0, 0, width, height);
 }
 
+unsigned int program;
+float Scale = 1.0f;
+
+void OnMouseScroll(GLFWwindow* pWindow, double x, double y)
+{
+    Scale -= y * 0.1f;
+    glUniform1f(glGetUniformLocation(program, "uScale"), Scale);
+}
+
 unsigned int CreateTriangle(unsigned int & vertexCount)
 {
     float vertices[] = {
-        //  (x, y, z, w) (r, g, b)
-        -0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,      // top right
-        -0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f,    // bottom left
-         0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,   // bottom right
+        //  (x, y, z, w)
+        -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,      // bottom left
+         0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 0.0f,     // bottom right
+         0.5f,  0.5f, 0.0f, 1.0f, 1.0f, 1.0f,     // top right
+        -0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 1.0f     // top left
+    };
 
-         0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,      // top left
-        -0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,      // top right
-         0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,    // bottom right
+    float colors[] = {
+         //  (r, g, b)
+        1.0f, 1.0f, 1.0f,     // bottom left
+        1.0f, 1.0f, 1.0f,     // bottom right
+        1.0f, 1.0f, 1.0f,     // top right
+        1.0f, 1.0f, 1.0f,     // top left
+    };
+
+    unsigned int indices[] = {
+        0, 1, 2,
+        0, 2, 3
     };
 
     vertexCount = 6;
@@ -27,17 +83,32 @@ unsigned int CreateTriangle(unsigned int & vertexCount)
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
     {
-        unsigned int VBO;
-        glGenBuffers(1, &VBO);
-        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        unsigned int VBOPos;
+        glGenBuffers(1, &VBOPos);
+        glBindBuffer(GL_ARRAY_BUFFER, VBOPos);
         glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
         // at location 0, read 4 floats and jump (stride) 4 floats for next vertex.
-        glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, (4+3) * sizeof(float), (void*)0);
+        glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, (6) * sizeof(float), (void*)0);
         glEnableVertexAttribArray(0);
 
-        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, (4+3) * sizeof(float), (void*)(4*sizeof(float)));
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, (6) * sizeof(float), (void*)(4*sizeof(float)));
         glEnableVertexAttribArray(1);
+
+        /*
+        unsigned int VBOCol;
+        glGenBuffers(1, &VBOCol);
+        glBindBuffer(GL_ARRAY_BUFFER, VBOCol);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, (3) * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(1);
+        */
+
+        unsigned int EBO;
+        glGenBuffers(1, &EBO);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     }
     glBindVertexArray(0);
 
@@ -49,13 +120,17 @@ unsigned int CreateShaders()
     const char * VertexShaderSource =
         "#version 330 core\n"
         "layout(location = 0) in vec4 aPos;\n"
-        "layout(location = 1) in vec3 Color;\n"
+        "layout(location = 1) in vec2 TexCoord;\n"
+        "//layout(location = 1) in vec3 Color;\n"
+        "uniform vec3 uColor;\n"
+        "uniform float uScale;\n"
+        "uniform float uTime;\n"
         "out vec3 colorFrag;\n"
-
+        "out vec2 UVFrag;\n"
         "void main()\n"
         "{\n"
-            "gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-            "colorFrag = Color;\n"
+            "gl_Position = vec4(aPos.x*uScale*uTime, aPos.y*uScale*uTime, aPos.z, 1.0);\n"
+            "UVFrag = TexCoord;\n"
         "}\n";
 
     unsigned int vertexShader;
@@ -74,11 +149,21 @@ unsigned int CreateShaders()
 
     const char* FragmentShaderSource =
         "#version 330 core\n"
-        "out vec4 FragColor;\n"
         "in vec3 colorFrag;\n"
+        "in vec2 UVFrag;\n"
+        "uniform vec3 uColor[2];\n"
+        "out vec4 FragColor;\n"
+        "uniform sampler2D Texture;\n"
+        "uniform sampler2D Second;\n"
         "void main()\n"
         "{\n"
-            "FragColor = vec4(colorFrag.r, colorFrag.g, colorFrag.b, 1.0f);\n"
+            "vec2 scaledUV = (UVFrag - vec2(0.5)) * vec2(2.0) + vec2(0.5);\n"
+            "vec2 scaledUV1 = UVFrag * vec2(2.0) - vec2(0.5);\n"
+            "vec4 colorMinionOne = texture(Texture, UVFrag);\n"
+            "//vec4 colorMinionTwo = texture(Second, UVFrag);\n"
+            "//vec4 colorMultiply = colorMinionOne* colorMinionTwo;\n"
+            "//vec4 finalColor = colorMinionOne*colorMinionOne.a + colorMinionTwo*(1.0 - colorMinionOne.a);\n"
+            "FragColor = colorMinionOne;\n"
         "}\n";
 
     unsigned int fragmentShader;
@@ -113,6 +198,54 @@ unsigned int CreateShaders()
     return shaderProgram;
 }
 
+unsigned int LoadTexture(std::string strFileName)
+{
+    int width, height, nrChannels;
+    stbi_set_flip_vertically_on_load(true);
+    unsigned char* data = stbi_load(strFileName.c_str(), &width, &height, &nrChannels, 0);
+
+    if (!data)
+    {
+        std::cout << "Failed to load the image :" << strFileName << std::endl;
+        return 0;
+    }
+
+    std::cout << "Image Name : " << strFileName << std::endl;
+    std::cout << "Image Width : " << width << std::endl;
+    std::cout << "Channels : " << nrChannels << std::endl;
+
+    GLint format = (nrChannels == 4) ? GL_RGBA : GL_RGB;
+
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+
+    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+
+    //float borderColor[] = { 1.0f, 1.0f, 0.0f, 1.0f };
+    //glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
+
+    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+    // Filtering ( Tri-linear filtering )
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    stbi_image_free(data);
+    return texture;
+}
+
 int main()
 {
     GLFWwindow* window;
@@ -121,8 +254,8 @@ int main()
     if (!glfwInit())
         return -1;
 
-    int width = 640;
-    int height = 480;
+    int width = 1920;
+    int height = 1080;
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -145,11 +278,15 @@ int main()
 
     glfwSetFramebufferSizeCallback(window, OnWindowResize);
 
+    glfwSetScrollCallback(window, OnMouseScroll);
+
     // TODO: Move to material
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);    // Transparent blend or Alpha blend or Transparent
     //glBlendFunc(GL_ONE, GL_ZERO); // Opaque or Replace blend
     //glBlendFunc(GL_ONE_MINUS_SRC_COLOR, GL_ZERO);
+
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     glEnable(GL_CULL_FACE);
     glCullFace(GL_BACK);
@@ -165,7 +302,15 @@ int main()
     unsigned int vertexCount = 0;
     // Entities
     unsigned int triangle = CreateTriangle(vertexCount);
-    unsigned int program = CreateShaders();
+    program = CreateShaders();
+
+    glUseProgram(program);
+    glUniform1f(glGetUniformLocation(program, "uScale"), 1.0f);
+
+    //unsigned int texture = LoadTexture(GetTexturePath() + "small_texture.png");
+    unsigned int texture = LoadTexture(GetTexturePath() + "minions\\minion.png");
+    unsigned int texture2 = LoadTexture(GetTexturePath() + "minions\\minion.jpg");
+
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -177,10 +322,28 @@ int main()
         if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
             glfwSetWindowShouldClose(window, true);
 
+
+        float time = (float)glfwGetTime();
+        float fTime = 1.0f + fabs(sinf(time*0.25f)*2.0f);
+        float fCos = cosf(time * 0.2f);
+        glUniform1f(glGetUniformLocation(program, "uTime"), 1.0f);
+        //glUniform1f(glGetUniformLocation(program, "uScale"), 0.5f);
+
+        float color[] = { 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f };
+        glUniform3fv(glGetUniformLocation(program, "uColor"), 2, color);
+
         glUseProgram(program);
         glBindVertexArray(triangle);
+        glActiveTexture(GL_TEXTURE0); // activate the texture unit first before binding texture
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glActiveTexture(GL_TEXTURE1); // activate the texture unit first before binding texture
+        glBindTexture(GL_TEXTURE_2D, texture2);
 
-        glDrawArrays(GL_TRIANGLES, 0, vertexCount);
+        glUniform1i(glGetUniformLocation(program, "Texture"), 0); // set it manually
+        glUniform1i(glGetUniformLocation(program, "Second"), 1); // set it manually
+
+        //glDrawArrays(GL_TRIANGLES, 0, vertexCount);
+        glDrawElements(GL_TRIANGLES, vertexCount /* index count */, GL_UNSIGNED_INT, 0);
 
         glBindVertexArray(0);
 
