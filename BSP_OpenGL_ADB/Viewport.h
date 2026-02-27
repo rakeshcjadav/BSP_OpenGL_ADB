a706756412b0897ceaa3e026573f15dd03cdee0d
Rakesh Jadav <rakeshcjadav@gmail.com>
2026-02-27 23:50:42 +0530

    Upgrade the SDK

--- BSP_OpenGL_ADB/Viewport.h
+++ BSP_OpenGL_ADB/Viewport.h
@@ -2,6 +2,13 @@
 
 class CCamera;
 
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
+#include "glm/gtx/string_cast.hpp"
+#include "glm/gtx/quaternion.hpp"
+
+
 class CViewport
 {
 public:
